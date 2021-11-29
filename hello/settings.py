05a6f81e30a1ec1af4f73eb550128ce5ab621d3d
Abdul Rehman <shahidabdulrehman03@gmail.com>
2021-11-29 14:52:34 +0500

    Update settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sq0qd=pqf6yfl&@soj9y2v5wr@6wyrjaf^+v2ixlf%-f@!e_^6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'hello-vr.herokuapp.com']
 
