8469add044cc2c0b922aa2c7dbe70f18047cf02b
ars-1 <shahidabdulrehman03@gmail.com>
2021-11-29 14:32:33 +0500

    Hello updated

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILESDIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     ]
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
